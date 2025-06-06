5eb6ba6aa6bb0ecfa3a14970f65e376a0c83cd30
Sailendra <sailendra.maharjan@rumsan.net>
2025-06-06 20:59:27 +0545

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'camera',
+  webDir: 'dist'
+};
+
+export default config;
